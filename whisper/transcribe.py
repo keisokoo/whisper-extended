5d805bf6a9750c8cbb59a2849e7757246bd6f6c9
keisokoo <keisokoo@gmail.com>
2024-11-18 17:29:04 +0900

    log

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -488,9 +488,6 @@ def transcribe(
                     if callback_segment:
                         callback_segment(segment)
                         
-                if callback_segment:
-                    callback_segment(None)
-
             # if a segment is instantaneous or does not contain text, clear it
             for i, segment in enumerate(current_segments):
                 if segment["start"] == segment["end"] or segment["text"].strip() == "":
@@ -516,6 +513,9 @@ def transcribe(
 
             # update progress bar
             pbar.update(min(content_frames, seek) - previous_seek)
+            
+    if callback_segment:
+        callback_segment(None)
 
     return dict(
         text=tokenizer.decode(all_tokens[len(initial_prompt_tokens) :]),
